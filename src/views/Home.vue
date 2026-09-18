d4fc3c320d5c7f5d835e1dd9183dcd7eeca31636
Vadim Zavgorodniy <lostprophetsco@gmail.com>
2026-09-18 10:18:17 +0300

    Revert "Init"
    This reverts commit 386b4c1a6ab8ff92e7c92c465db8f34d70d4227a.

--- src/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="home">
-    <h1>Каталог книг</h1>
-    <p class="text-muted">Добро пожаловать в каталог книг</p>
-  </div>
-</template>
-
-<script setup lang="ts">
-</script>
